12e822fbfe64fcdfb5c74f14b308c83cd1d8f7a6
haoxiaoping <haoxiaoping@staff.hexun.com>
2019-04-09 14:17:31 +0800

    机制调整

--- src/org/wltea/analyzer/dic/Dictionary.java
+++ src/org/wltea/analyzer/dic/Dictionary.java
@@ -42,56 +42,52 @@ import org.wltea.analyzer.cfg.Configuration;
 import org.wltea.analyzer.cfg.DefaultConfig;
 import org.wltea.analyzer.cfg.PropertiesMethodUtil;
 
-
 /**
  * 词典管理类,单子模式
  */
 public class Dictionary {
 
-
 	/*
 	 * 词典单子实例
 	 */
 	private static Dictionary singleton;
-	
+
 	/*
 	 * 主词典对象
 	 */
 	private DictSegment _MainDict;
-	
+
 	/*
-	 * 停止词词典 
+	 * 停止词词典
 	 */
 	private DictSegment _StopWordDict;
 	/*
 	 * 量词词典
 	 */
 	private DictSegment _QuantifierDict;
-	 
+
 	/**
 	 * 配置对象
 	 */
 	private Configuration cfg;
-	
-	private Dictionary(Configuration cfg){
+
+	private Dictionary(Configuration cfg) {
 		this.cfg = cfg;
 		this.loadMainDict();
 		this.loadStopWordDict();
 		this.loadQuantifierDict();
 	}
-	
+
 	/**
-	 * 词典初始化
-	 * 由于IK Analyzer的词典采用Dictionary类的静态方法进行词典初始化
-	 * 只有当Dictionary类被实际调用时，才会开始载入词典，
-	 * 这将延长首次分词操作的时间
-	 * 该方法提供了一个在应用加载阶段就初始化字典的手段
+	 * 词典初始化 由于IK Analyzer的词典采用Dictionary类的静态方法进行词典初始化
+	 * 只有当Dictionary类被实际调用时，才会开始载入词典， 这将延长首次分词操作的时间 该方法提供了一个在应用加载阶段就初始化字典的手段
+	 * 
 	 * @return Dictionary
 	 */
-	public static Dictionary initial(Configuration cfg){
-		if(singleton == null){
-			synchronized(Dictionary.class){
-				if(singleton == null){
+	public static Dictionary initial(Configuration cfg) {
+		if (singleton == null) {
+			synchronized (Dictionary.class) {
+				if (singleton == null) {
 					singleton = new Dictionary(cfg);
 					return singleton;
 				}
@@ -99,118 +95,125 @@ public class Dictionary {
 		}
 		return singleton;
 	}
-	
+
 	/**
 	 * 获取词典单子实例
+	 * 
 	 * @return Dictionary 单例对象
 	 */
-	public static Dictionary getSingleton(){
-		if(singleton == null){
+	public static Dictionary getSingleton() {
+		if (singleton == null) {
 			throw new IllegalStateException("词典尚未初始化，请先调用initial方法");
 		}
 		return singleton;
 	}
-	
+
 	/**
 	 * 批量加载新词条
-	 * @param words Collection<String>词条列表
+	 * 
+	 * @param words
+	 *            Collection<String>词条列表
 	 */
-	public void addWords(Collection<String> words){
-		if(words != null){
-			for(String word : words){
+	public void addWords(Collection<String> words) {
+		if (words != null) {
+			for (String word : words) {
 				if (word != null) {
-					//批量加载词条到主内存词典中
+					// 批量加载词条到主内存词典中
 					singleton._MainDict.fillSegment(word.trim().toLowerCase().toCharArray());
 				}
 			}
 		}
 	}
-	
+
 	/**
 	 * 批量移除（屏蔽）词条
+	 * 
 	 * @param words
 	 */
-	public void disableWords(Collection<String> words){
-		if(words != null){
-			for(String word : words){
+	public void disableWords(Collection<String> words) {
+		if (words != null) {
+			for (String word : words) {
 				if (word != null) {
-					//批量屏蔽词条
+					// 批量屏蔽词条
 					singleton._MainDict.disableSegment(word.trim().toLowerCase().toCharArray());
 				}
 			}
 		}
 	}
-	
+
 	/**
 	 * 检索匹配主词典
+	 * 
 	 * @param charArray
 	 * @return Hit 匹配结果描述
 	 */
-	public Hit matchInMainDict(char[] charArray){
+	public Hit matchInMainDict(char[] charArray) {
 		return singleton._MainDict.match(charArray);
 	}
-	
+
 	/**
 	 * 检索匹配主词典
+	 * 
 	 * @param charArray
 	 * @param begin
 	 * @param length
 	 * @return Hit 匹配结果描述
 	 */
-	public Hit matchInMainDict(char[] charArray , int begin, int length){
+	public Hit matchInMainDict(char[] charArray, int begin, int length) {
 		return singleton._MainDict.match(charArray, begin, length);
 	}
-	
+
 	/**
 	 * 检索匹配量词词典
+	 * 
 	 * @param charArray
 	 * @param begin
 	 * @param length
 	 * @return Hit 匹配结果描述
 	 */
-	public Hit matchInQuantifierDict(char[] charArray , int begin, int length){
+	public Hit matchInQuantifierDict(char[] charArray, int begin, int length) {
 		return singleton._QuantifierDict.match(charArray, begin, length);
 	}
-	
-	
+
 	/**
 	 * 从已匹配的Hit中直接取出DictSegment，继续向下匹配
+	 * 
 	 * @param charArray
 	 * @param currentIndex
 	 * @param matchedHit
 	 * @return Hit
 	 */
-	public Hit matchWithHit(char[] charArray , int currentIndex , Hit matchedHit){
+	public Hit matchWithHit(char[] charArray, int currentIndex, Hit matchedHit) {
 		DictSegment ds = matchedHit.getMatchedDictSegment();
-		return ds.match(charArray, currentIndex, 1 , matchedHit);
+		return ds.match(charArray, currentIndex, 1, matchedHit);
 	}
-	
-	
+
 	/**
 	 * 判断是否是停止词
+	 * 
 	 * @param charArray
 	 * @param begin
 	 * @param length
 	 * @return boolean
 	 */
-	public boolean isStopWord(char[] charArray , int begin, int length){			
+	public boolean isStopWord(char[] charArray, int begin, int length) {
 		return singleton._StopWordDict.match(charArray, begin, length).isMatch();
-	}	
-	
+	}
+
 	/**
 	 * 加载主词典及扩展词典
 	 */
-	private void loadMainDict(){
-		//建立一个主词典实例
-		_MainDict = new DictSegment((char)0);
-		//读取主词典文件
-        InputStream is = this.getClass().getClassLoader().getResourceAsStream(cfg.getMainDictionary());
-        if(is == null){
-        	throw new RuntimeException("Main Dictionary not found!!!");
-        }
-        
+	private void loadMainDict() {
+		// 建立一个主词典实例
+		_MainDict = new DictSegment((char) 0);
+		// 读取主词典文件
+		InputStream is = this.getClass().getClassLoader().getResourceAsStream(cfg.getMainDictionary());
+		if (is == null) {
+			throw new RuntimeException("Main Dictionary not found!!!");
+		}
+
 		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(is , "UTF-8"), 512);
+			BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"), 512);
 			String theWord = null;
 			do {
 				theWord = br.readLine();
@@ -218,49 +221,42 @@ public class Dictionary {
 					_MainDict.fillSegment(theWord.trim().toLowerCase().toCharArray());
 				}
 			} while (theWord != null);
-			
+
 		} catch (IOException ioe) {
 			System.err.println("Main Dictionary loading exception.");
 			ioe.printStackTrace();
-			
-		}finally{
+
+		} finally {
 			try {
-				if(is != null){
-                    is.close();
-                    is = null;
+				if (is != null) {
+					is.close();
+					is = null;
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		//加载扩展词典
+		// 加载扩展词典
 		this.loadExtDict(_MainDict);
 		// 从mysql加载词典
 		this.loadMySQLExtDict(_MainDict);
-		
+
 		new Thread(new HotDictReloadThread()).start();
-	}	
-	
+	}
+
 	/**
 	 * 加载主词典及扩展词典
 	 */
-	public void reLoadMainDict(){
-		try {//先休眠 
-			Thread.sleep(Integer.valueOf(String.valueOf(PropertiesMethodUtil.getProperties("jdbc-reload.properties").get("jdbc.reload.interval"))));
-		} catch (NumberFormatException e1) { 
-			e1.printStackTrace();
-		} catch (InterruptedException e1) { 
-			e1.printStackTrace();
-		} 
-		DictSegment reload_MainDict = new DictSegment((char)0);
-		//读取主词典文件
-        InputStream is = this.getClass().getClassLoader().getResourceAsStream(cfg.getMainDictionary());
-        if(is == null){
-        	throw new RuntimeException("Main Dictionary not found!!!");
-        }
-        
+	public void reLoadMainDict() {
+		DictSegment reload_MainDict = new DictSegment((char) 0);
+		// 读取主词典文件
+		InputStream is = this.getClass().getClassLoader().getResourceAsStream(cfg.getMainDictionary());
+		if (is == null) {
+			throw new RuntimeException("Main Dictionary not found!!!");
+		}
+
 		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(is , "UTF-8"), 512);
+			BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"), 512);
 			String theWord = null;
 			do {
 				theWord = br.readLine();
@@ -268,212 +264,208 @@ public class Dictionary {
 					reload_MainDict.fillSegment(theWord.trim().toLowerCase().toCharArray());
 				}
 			} while (theWord != null);
-			
+
 		} catch (IOException ioe) {
 			System.err.println("Main Dictionary loading exception.");
 			ioe.printStackTrace();
-			
-		}finally{
+
+		} finally {
 			try {
-				if(is != null){
-                    is.close();
-                    is = null;
+				if (is != null) {
+					is.close();
+					is = null;
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		//加载扩展词典
+		// 加载扩展词典
 		this.loadExtDict(reload_MainDict);
 		// 从mysql加载词典
 		this.loadMySQLExtDict(reload_MainDict);
-		
-		_MainDict=reload_MainDict;//从新加载
-	}	
-	
+
+		_MainDict = reload_MainDict;// 从新加载
+	}
+
 	/**
 	 * 从mysql加载热更新词典
 	 */
-	private void loadMySQLExtDict(DictSegment mainDict ) {
+	private void loadMySQLExtDict(DictSegment mainDict) {
 		Connection conn = null;
 		Statement stmt = null;
-		ResultSet rs = null;		
+		ResultSet rs = null;
 		try {
-			System.out.println("[==========]jdbc-reload.properties");	
-			Properties prop=PropertiesMethodUtil.getProperties("jdbc-reload.properties");  
-			String jdbc_isrun=prop.getProperty("jdbc.isrun");
-			if(jdbc_isrun==null ||jdbc_isrun.equals("0")) 
-			{
-				return;//此处判断是否启用mysql分词
+			System.out.println("[==========]jdbc-reload.properties");
+			Properties prop = PropertiesMethodUtil.getProperties("jdbc-reload.properties");
+			for (Object key : prop.keySet()) {
+				System.out.println("[==========]" + key + "=" + prop.getProperty(String.valueOf(key)));
 			}
-			for(Object key : prop.keySet()) {
-				System.out.println("[==========]" + key + "=" + prop.getProperty(String.valueOf(key)));      
-			}			
-			System.out.println("[==========]query hot dict from mysql, " + prop.getProperty("jdbc.reload.sql") + "......"); 
+			System.out.println(
+					"[==========]query hot dict from mysql, " + prop.getProperty("jdbc.reload.sql") + "......");
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			conn = DriverManager.getConnection(
-					prop.getProperty("jdbc.url"),   
-					prop.getProperty("jdbc.user"),  
-					prop.getProperty("jdbc.password"));  
+			conn = DriverManager.getConnection(prop.getProperty("jdbc.url"), prop.getProperty("jdbc.user"),
+					prop.getProperty("jdbc.password"));
 			stmt = conn.createStatement();
-			String sqlsize=prop.getProperty("jdbc.reload.sqlsize");
-			Integer nsize=Integer.valueOf(sqlsize);
-			String sql=prop.getProperty("jdbc.reload.sql");
-			int ncount=0;
-			String lastid="0";
-			int count_n=0;
-			do{
-				ncount=0;
+			String sqlsize = prop.getProperty("jdbc.reload.sqlsize");
+			Integer nsize = Integer.valueOf(sqlsize);
+			String sql = prop.getProperty("jdbc.reload.sql");
+			int ncount = 0;
+			String lastid = "0";
+			int count_n = 0;
+			do {
+				ncount = 0;
 				count_n++;
-				String sqlss=String.format(sql,lastid,sqlsize);
-				if(count_n%100==0)
-					System.out.println("[==========]sqlss*******, count_n:" +count_n+"________:::::"+ sqlss+ "......"); 
-				rs = stmt.executeQuery(sqlss);					
-				while(rs.next()) {
+				String sqlss = String.format(sql, lastid, sqlsize);
+				if (count_n % 100 == 0)
+					System.out.println(
+							"[==========]sqlss*******, count_n:" + count_n + "________:::::" + sqlss + "......");
+				rs = stmt.executeQuery(sqlss);
+				while (rs.next()) {
 					ncount++;
-					String theWord = rs.getString(1); 					
-					lastid= rs.getString(2); 
-					System.out.println(theWord); 
-					mainDict.fillSegment(theWord.trim().toCharArray());
-				} 
-			}
-			while(ncount==nsize);			
-			System.out.println("[==========]reload end......"); 
+					String theWord = rs.getString(1);
+					if (theWord != null && !theWord.trim().equals("")) {
+						lastid = rs.getString(2);
+						// System.out.println(theWord);
+						mainDict.fillSegment(theWord.trim().toCharArray());
+					}
+				}
+			} while (ncount == nsize);
+			System.out.println("[==========]reload end......");
 		} catch (Exception e) {
-			System.err.println( e); 
+			System.err.println(e);
 		} finally {
-			if(rs != null) {
+			if (rs != null) {
 				try {
 					rs.close();
 				} catch (SQLException e) {
-					System.err.println(e); 
+					System.err.println(e);
 				}
 			}
-			if(stmt != null) {
+			if (stmt != null) {
 				try {
 					stmt.close();
 				} catch (SQLException e) {
-					System.err.println( e); 
+					System.err.println(e);
 				}
 			}
-			if(conn != null) {
+			if (conn != null) {
 				try {
 					conn.close();
 				} catch (SQLException e) {
-					System.err.println(e); 
+					System.err.println(e);
 				}
 			}
 		}
-		System.out.println("[==========]sql end....");  		
+		System.out.println("[==========]sql end....");
 	}
-	
+
 	/**
 	 * 加载用户配置的扩展词典到主词库表
 	 */
-	private void loadExtDict( DictSegment mainDict){
-		//加载扩展词典配置
-		List<String> extDictFiles  = cfg.getExtDictionarys();
-		if(extDictFiles != null){
+	private void loadExtDict(DictSegment mainDict) {
+		// 加载扩展词典配置
+		List<String> extDictFiles = cfg.getExtDictionarys();
+		if (extDictFiles != null) {
 			InputStream is = null;
-			for(String extDictName : extDictFiles){
-				//读取扩展词典文件
+			for (String extDictName : extDictFiles) {
+				// 读取扩展词典文件
 				System.out.println("加载扩展词典：" + extDictName);
 				is = this.getClass().getClassLoader().getResourceAsStream(extDictName);
-				//如果找不到扩展的字典，则忽略
-				if(is == null){
+				// 如果找不到扩展的字典，则忽略
+				if (is == null) {
 					continue;
 				}
 				try {
-					BufferedReader br = new BufferedReader(new InputStreamReader(is , "UTF-8"), 512);
+					BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"), 512);
 					String theWord = null;
 					do {
 						theWord = br.readLine();
 						if (theWord != null && !"".equals(theWord.trim())) {
-							//加载扩展词典数据到主内存词典中
-							//System.out.println(theWord);
+							// 加载扩展词典数据到主内存词典中
+							// System.out.println(theWord);
 							mainDict.fillSegment(theWord.trim().toLowerCase().toCharArray());
 						}
 					} while (theWord != null);
-					
+
 				} catch (IOException ioe) {
 					System.err.println("Extension Dictionary loading exception.");
 					ioe.printStackTrace();
-					
-				}finally{
+
+				} finally {
 					try {
-						if(is != null){
-		                    is.close();
-		                    is = null;
+						if (is != null) {
+							is.close();
+							is = null;
 						}
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
 				}
 			}
-		}		
+		}
 	}
-	
+
 	/**
 	 * 加载用户扩展的停止词词典
 	 */
-	private void loadStopWordDict(){
-		//建立一个主词典实例
-		_StopWordDict = new DictSegment((char)0);
-		//加载扩展停止词典
-		List<String> extStopWordDictFiles  = cfg.getExtStopWordDictionarys();
-		if(extStopWordDictFiles != null){
+	private void loadStopWordDict() {
+		// 建立一个主词典实例
+		_StopWordDict = new DictSegment((char) 0);
+		// 加载扩展停止词典
+		List<String> extStopWordDictFiles = cfg.getExtStopWordDictionarys();
+		if (extStopWordDictFiles != null) {
 			InputStream is = null;
-			for(String extStopWordDictName : extStopWordDictFiles){
+			for (String extStopWordDictName : extStopWordDictFiles) {
 				System.out.println("加载扩展停止词典：" + extStopWordDictName);
-				//读取扩展词典文件
+				// 读取扩展词典文件
 				is = this.getClass().getClassLoader().getResourceAsStream(extStopWordDictName);
-				//如果找不到扩展的字典，则忽略
-				if(is == null){
+				// 如果找不到扩展的字典，则忽略
+				if (is == null) {
 					continue;
 				}
 				try {
-					BufferedReader br = new BufferedReader(new InputStreamReader(is , "UTF-8"), 512);
+					BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"), 512);
 					String theWord = null;
 					do {
 						theWord = br.readLine();
 						if (theWord != null && !"".equals(theWord.trim())) {
-							//System.out.println(theWord);
-							//加载扩展停止词典数据到内存中
+							// System.out.println(theWord);
+							// 加载扩展停止词典数据到内存中
 							_StopWordDict.fillSegment(theWord.trim().toLowerCase().toCharArray());
 						}
 					} while (theWord != null);
-					
+
 				} catch (IOException ioe) {
 					System.err.println("Extension Stop word Dictionary loading exception.");
 					ioe.printStackTrace();
-					
-				}finally{
+
+				} finally {
 					try {
-						if(is != null){
-		                    is.close();
-		                    is = null;
+						if (is != null) {
+							is.close();
+							is = null;
 						}
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
 				}
 			}
-		}		
+		}
 	}
-	
+
 	/**
 	 * 加载量词词典
 	 */
-	private void loadQuantifierDict(){
-		//建立一个量词典实例
-		_QuantifierDict = new DictSegment((char)0);
-		//读取量词词典文件
-        InputStream is = this.getClass().getClassLoader().getResourceAsStream(cfg.getQuantifierDicionary());
-        if(is == null){
-        	throw new RuntimeException("Quantifier Dictionary not found!!!");
-        }
+	private void loadQuantifierDict() {
+		// 建立一个量词典实例
+		_QuantifierDict = new DictSegment((char) 0);
+		// 读取量词词典文件
+		InputStream is = this.getClass().getClassLoader().getResourceAsStream(cfg.getQuantifierDicionary());
+		if (is == null) {
+			throw new RuntimeException("Quantifier Dictionary not found!!!");
+		}
 		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(is , "UTF-8"), 512);
+			BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"), 512);
 			String theWord = null;
 			do {
 				theWord = br.readLine();
@@ -481,25 +473,25 @@ public class Dictionary {
 					_QuantifierDict.fillSegment(theWord.trim().toLowerCase().toCharArray());
 				}
 			} while (theWord != null);
-			
+
 		} catch (IOException ioe) {
 			System.err.println("Quantifier Dictionary loading exception.");
 			ioe.printStackTrace();
-			
-		}finally{
+
+		} finally {
 			try {
-				if(is != null){
-                    is.close();
-                    is = null;
+				if (is != null) {
+					is.close();
+					is = null;
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		Dictionary.initial(DefaultConfig.getInstance());
 	}
-	
+
 }
--- src/org/wltea/analyzer/dic/HotDictReloadThread.java
+++ src/org/wltea/analyzer/dic/HotDictReloadThread.java
@@ -1,17 +1,25 @@
 package org.wltea.analyzer.dic;
 
+import org.wltea.analyzer.cfg.PropertiesMethodUtil;
+
 public class HotDictReloadThread implements Runnable {
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		while(true) {
-			try {
-				Thread.sleep(180000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			String jdbc_isrun=PropertiesMethodUtil.getProperties("jdbc-reload.properties").getProperty("jdbc.isrun");
+			if(jdbc_isrun==null ||jdbc_isrun.equals("0")) 
+			{
+				return;//此处判断是否启用mysql分词
 			}
+			try {//先休眠 
+				Thread.sleep(Integer.valueOf(String.valueOf(PropertiesMethodUtil.getProperties("jdbc-reload.properties").get("jdbc.reload.interval"))));
+			} catch (NumberFormatException e1) { 
+				e1.printStackTrace();
+			} catch (InterruptedException e1) { 
+				e1.printStackTrace();
+			} 
 			System.out.println("[==========]reload hot dict from mysql......");   
 			Dictionary.getSingleton().reLoadMainDict();
 		}
